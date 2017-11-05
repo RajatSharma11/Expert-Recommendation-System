bd0e34a35bc6688384bb5045d812d4b1f84402fa
Rajat Sharma <rajat4803@gmail.com>
2017-11-05 15:12:21 +0530

    Recommender.py file name changed to Recommender_v1.0.py and Recommender_v1.1.py file added which uses max function for getting tfidf score

---
+++ Recommender_v1.1.py
@@ -0,0 +1,95 @@
+import os
+import re
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+def mean(a):
+    return sum(a) / len(a)
+input_path = "title_abstract_data"
+expr = re.compile("[a-zA-Z]+")
+tokenize = lambda doc: expr.findall(doc)
+files = next(os.walk(input_path))[2]
+reviwer_title_dic = {}
+reviwer_abstract_dic = {}
+reviwer_keyword_dic = {}
+for i in range(len(files)):
+	l = []
+	m = []
+	f = open("title_abstract_data/" + files[i],"r", encoding='utf8',  errors='ignore')
+	for line in f:
+		s = line.split("|")
+		l.append(s[0])
+		if(len(s[1]) > 1):
+			m.append(s[1])
+	reviwer_title_dic[files[i][:-4]] = l
+	reviwer_abstract_dic[files[i][:-4]] = m
+	while("" in m):
+		m.remove("")
+	if len(m) > 0:
+		sklearn_tfidf = TfidfVectorizer(norm='l2',min_df=0, use_idf=True, smooth_idf=True, sublinear_tf=False, tokenizer=tokenize)
+		sklearn_representation = sklearn_tfidf.fit_transform(m)
+	reviwer_Score_dic = {}
+	vect = np.max(sklearn_representation.todense(), axis=0).tolist()
+	for key in sklearn_tfidf.vocabulary_:
+		reviwer_Score_dic[key] = vect[0][sklearn_tfidf.vocabulary_[key]]
+	reviwer_Score_dic = sorted(reviwer_Score_dic.items(), key=lambda x: x[1])
+	#print(reviwer_Score_dic)	
+	g = []
+	n = int(len(reviwer_Score_dic) * 0.2)
+	for f in range(n):
+		item = reviwer_Score_dic.pop()
+		key,value = item
+		g.append(key)
+	reviwer_keyword_dic[files[i][:-4]] = g 
+
+input_title = open("Expert_Finding_Data/titles.txt","r", encoding='utf8',  errors='ignore')
+input_abstract = open("Expert_Finding_Data/abstracts.txt","r", encoding='utf8',  errors='ignore')
+
+l = []
+m = []
+v = 1		# for indexing in input_dic
+writers_name = []
+writers_name_list = []
+input_dic = {}
+for line in input_title:
+	s = line.split("/")
+	l.append(s[0])
+	writers_name = "".join(s[1]).split("and")
+	for i in range(len(writers_name)):
+		if("\n" in writers_name[i]):
+			writers_name[i] = writers_name[i][:-1]
+		writers_name[i] = writers_name[i].strip(" ")
+	while("" in writers_name):
+		writers_name.remove("")
+	writers_name_list.append(set(writers_name))
+	print(writers_name_list)
+
+for line in input_abstract:
+	s = l[v - 1] + line 
+	input_dic[v] = tokenize(s) 
+	v += 1
+
+def jaccard_similarity(query, document):
+    intersection = set(query).intersection(set(document))
+    union = set(query).union(set(document))
+    return len(intersection)/len(union)
+
+
+index = 1
+final_dic = {}
+for name in writers_name_list:
+	jaccard_dic = {}
+	for reviewer in reviwer_keyword_dic:
+		if(len(name.intersection(set(reviewer))) > 0):
+			continue;
+		else:
+			jaccard_dic[reviewer] = jaccard_similarity(input_dic[index],reviwer_keyword_dic[reviewer])
+	jaccard_dic = sorted(jaccard_dic.items(), key=lambda x: x[1]))
+	g = []
+	n = 10			# number of reviewers to be assigned
+	for f in range(n):
+		item = jaccard_dic.pop()
+		key,value = item
+		g.append(key)
+	final_dic[index] = g
+	index += 1
+print(final_dic)
